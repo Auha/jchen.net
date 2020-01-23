29656bafa056f75d03a601f2dc5d7663a89ecccc
Jonathan <jonathan.chen@sunrider.com>
2020-01-23 16:48:01 -0700

    upgrades

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './blog/blog.module#BlogModule'}
+  {path: '', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
